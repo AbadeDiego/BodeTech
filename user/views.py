33438fb5a7e5f25807928eb18364bd1054b837cf
Felipe <felipecrs32@gmail.com>
2022-02-24 19:15:52 -0300

    edit: views.py

--- user/views.py
+++ user/views.py
@@ -88,7 +88,7 @@ def getFiles(path):
         if file.endswith(".png"):
             images.append(os.path.join(path, file))
             
-filesPath = "/static/images/fidelity"
+filesPath = "\\static\\images\\fidelity"
 
 getFiles(filesPath)
 
