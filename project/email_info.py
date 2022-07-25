389d86201de96358047e0728fd35b31e2f06cd34
Diego Freire <freirediego128@gmail.com>
2022-07-25 09:51:55 -0300

    make it better

--- project/email_info.py
+++ project/email_info.py
@@ -2,7 +2,7 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER='efidelity01@gmail.com'
-EMAIL_HOST_PASSWORD='22&fidelity'
+EMAIL_HOST_PASSWORD='&f1d&l1ty'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 """
